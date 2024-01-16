20b0d5ef1d0413a07ecd1197f5b91f08622f4ca9
EErickson <avilasonson@gmail.com>
2024-01-16 19:24:12 +0800

    refactor route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 //import Navbar from './Components/Navbar/navbar';
-import Login from './Pages/Login';
 import AdminPanel from './Pages/AdminPage/AdminPanel';
+import NotesPanel from './Pages/NotesPanel';
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Login/>}/>
+          <Route path='/' element={<NotesPanel/>}/>
           <Route path='/admin/panel' element={<AdminPanel/>}/>
         </Routes>
       </BrowserRouter>
