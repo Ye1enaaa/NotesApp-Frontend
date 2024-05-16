2ca5c51750513c99f30616d034c04c4a220a7fd6
Ye1enaaa <avilasonson@gmail.com>
2024-05-16 11:11:34 +0800

    add get time greetings

--- src/Components/LoginComponent/LoginForm.jsx
+++ src/Components/LoginComponent/LoginForm.jsx
@@ -38,6 +38,7 @@ const handleSubmit = (e) => {
         }
     })
 }
+
 return (
     <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-full max-w-sm">
           <h2 className="text-2xl font-bold mb-4">Welcome Back</h2>
