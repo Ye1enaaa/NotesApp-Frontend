d957f9472d638f7006a05fa344833a48fc47fb96
Ye1enaaa <avilasonson@gmail.com>
2024-06-15 10:17:34 +0800

    separate api functions

---
+++ src/API/api.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+import dotenv from 'react-dotenv';
+
+
+export async function authenticateUser(userLoginCredentials:UserLoginCredentials):Promise<void> {
+    try{
+        const response = await axios.post(`${dotenv.API_URL}/auth/jwt/create/`, userLoginCredentials)
+        const token = response.data.access;
+        if(response.status === 200){
+            document.cookie = `token=${token}; path=/; max-age=3600;`;
+            console.log(response.status, response.data.access)
+            window.location.href = '/notes'
+        }else if(response.status === 401){
+            window.alert('Wrong Credentials, Please double check the credentials you input!!')
+        }
+    }catch(error){
+        console.error('Error during authentication', error)
+    }
+}
+
+export async function addUserNotes(userNotes: UserNotes, token: Token):Promise<void> {
+    axios.post(`${dotenv.API_URL}/post/add-posts`, userNotes, {
+        headers:{
+          'Authorization': `Bearer ${token}`
+        }
+      }).then((response) => {
+        if(response.status === 201 || response.status === 200){
+          console.log(response.data, 'Success')
+        }
+      });
+}
